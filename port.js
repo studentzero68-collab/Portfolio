c7671efe8e068cb22796d7296e9b60fd28514a0a
Mukelani <studentzero68@gmail.com>
2026-05-26 12:23:31 +0200

    I was really just correcting some code and the java , but I fought and finally got it to work and now I just need some touch ups and we are golden

---
+++ port.js
@@ -0,0 +1,12 @@
+const sliders = document.querySelectorAll('.slider');
+
+sliders.forEach((slider) => {
+    const slides = slider.querySelectorAll('.slide');
+    let index = 0;
+
+    setInterval(() => {
+        slides[index].classList.remove('active');
+        index = (index + 1) % slides.length;
+        slides[index].classList.add('active');
+    }, 3000);
+});
